ded23c9185894d27ec7bb75be0126ff106efef20
Joao Antonio Chagas Nunes <joaoantoniocn@gmail.com>
2018-04-09 21:42:06 -0300

    Primeiro Commit

---
+++ src/main.py
@@ -0,0 +1,10 @@
+import numpy as np
+import pandas as pd
+
+#base = open("../base/segmentation.test", "r")
+
+base = pd.read_csv('../base/segmentation.test')
+
+#print(base.readlines())
+print(base)
+
